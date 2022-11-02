48e5f5602dd5bf607292ec3f05085b5bf9a84067
FranciscoGiro <zekiko3@gmail.com>
2022-11-02 01:22:43 +0000

    line chart circle values

--- script.js
+++ script.js
@@ -32,9 +32,25 @@ function createScatterPlot(id, indicator) {
     .attr("id", `gScatterPlot-${id}`)
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
+    svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("font-size", "12px")
+    .attr("font-weight", "bold")
+    .attr("x", width)
+    .attr("y", height + margin.top + 10)
+    .text(indicator);
+
+    svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("font-size", "12px")
+    .attr("font-weight", "bold")
+    .attr("y", -margin.left+10)
+    .attr("x", -margin.top+20)
+    .text("Price Var %")
+
   d3.csv("./dataset/data.csv").then(function (data) {
 
-    console.log(data)
     const x = d3
       .scaleLinear()
       .domain(d3.extent(data, (d) => parseInt(d[indicator])))
@@ -66,8 +82,8 @@ function createScatterPlot(id, indicator) {
       .attr("cy", (d) => y(d.priceVar))
       .attr("r", 2)
       .style("fill", "steelblue")
-      .on("mouseover", (event, d) => handleMouseOver(d))
-      .on("mouseleave", (event, d) => handleMouseLeave())
+      .on("mouseover", (event, d) => handleMouseOver(d.Company))
+      .on("mouseleave", (event, d) => handleMouseLeave(d.Company))
       .append("title")
       .text((d) => d.Company);
   });
@@ -114,8 +130,8 @@ function updateScatterPlot(id, indicator) {
             .attr("cy", (d) => y(0))
             .attr("r", 2)
             .style("fill", "steelblue")
-            .on("mouseover", (event, d) => handleMouseOver(d))
-            .on("mouseleave", (event, d) => handleMouseLeave())
+            .on("mouseover", (event, d) => handleMouseOver(d.Company))
+            .on("mouseleave", (event, d) => handleMouseLeave(d.Company))
           circles
             .transition()
             .duration(1000)
@@ -221,35 +237,105 @@ function createLineChart(id) {
         .y(d => y(parseInt(d.priceVar))) 
         (d[1])
     })
-    .on("mouseover", (event, d) => handleMouseOver(d))		
-    .on("mouseleave", (event, d) => handleLineChartMouseLeave(d, color(d[0])))			
+    .on("mouseover", (event, d) => handleMouseOver(d[0]))		
+    .on("mouseleave", (event, d) => handleLineChartMouseLeave(d[0], color(d[0])))			
     .on("click", (event, d) => handleLineChartMouseClick(d))
     .append("title")
     .text((d) => d[0]);
 
     svg
-      .selectAll("circle.circleValues") 
+      .selectAll("circle.lineValues") 
       .data(data, (d) => d.Company) 
       .join("circle")
-      .attr("class", "circleValues")
+      .attr("class", "lineValues")
       .attr("cx", (d) => x(d.year))
       .attr("cy", (d) => y(parseInt(d.priceVar)))
       .attr("r", 2)
       .style("fill", d => color(d.Company))
-      .on("mouseover", (event, d) => handleMouseOver(d))
-      .on("mouseleave", (event, d) => handleLineChartMouseLeave(d, color(d[0])))
+      .on("mouseover", (event, d) => handleMouseOver(d.Company))
+      .on("mouseleave", (event, d) => handleLineChartMouseLeave(d.Company, color(d.Company)))
       .append("title")
       .text((d) => `${d.Company}\n${Number(d.priceVar).toFixed(2)}`);
   });
 }
 
 
+function updateLineChart(id) {
+  const margin = { top: 20, right: 30, bottom: 40, left: 30 };
+  const width = 250 - margin.left - margin.right;
+  const height = 300 - margin.top - margin.bottom;
+
+  d3.csv("./dataset/data.csv").then(function (data) {
+
+    const color = d3.scaleOrdinal()
+      .range(['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999'])
+
+    data = data.filter(d => defaultCompanies.includes(d.Company) )
+
+    const sumstat = d3.group(data, d => d.Company);
+
+    const svg = d3.select("#gLineChart");
+
+    // Create the X axis:
+    var x = d3.scaleLinear()
+      .range([0,width])
+      .domain(d3.extent(data, d => parseInt(d.priceVar))) ;
+    svg.selectAll(".myXaxis")
+      .call(d3.axisBottom(x).ticks(5).tickFormat((x) => x));
+
+    // create the Y axis
+    const y = d3.scaleLinear()
+        .domain(d3.extent(data, d => parseInt(d.priceVar)))
+        .range([height, 0]);
+    var yAxis = d3.axisLeft().scale(y);
+    
+    svg.selectAll(".myYaxis")
+      .call(yAxis);
+
+    // Create a update selection: bind to the new data
+    var u = svg.selectAll(".lineTest")
+      .data(sumstat);
+
+    // Updata the line
+    u
+      .join(
+        (enter) => {
+        circles = enter
+        .append("path")
+        .attr("class","lineTest")
+        .merge(u)
+        .attr("d", function(d){
+                return d3.line()
+                  .x((d) => x(d.year))
+                  .y((d) => y(parseInt(d.priceVar)))
+                  (d[1])
+              })
+        .attr("fill", "none")
+        .attr("stroke", function(d){ return color(d[0]) })
+        .attr("stroke-width", 1.5)
+        .on("mouseover", (event, d) => handleMouseOver(d))		
+        .on("mouseleave", (event, d) => handleLineChartMouseLeave(d, color(d[0])))			
+        },
+        (update) => {
+          update
+            .transition()
+            .duration(1000)
+            .attr("cx", (d) => x(d.year))
+            .attr("cy", (d) => y(parseInt(d.priceVar)))
+            .attr("r", 4);
+        },
+        (exit) => {
+            exit.remove();
+          }
+      )
+      ;
+  });
+}
+
 
 // Handle Events
 
-function handleMouseOver(item) {
-  company = item.Company || item[0]
-
+function handleMouseOver(company) {
   zIndex(company)
 
   d3.selectAll(".itemValue")
@@ -259,6 +345,12 @@ function handleMouseOver(item) {
     .attr("r", 4)
     .style("fill", "red");
 
+    d3.selectAll(".lineValues")
+    .filter(d => d.Company == company)
+    .style("fill", "black")
+    .attr("r", 2);
+
+
     d3.selectAll(".lineTest")
     .filter(function (d, i) {
       return d[0] == company;
@@ -267,16 +359,36 @@ function handleMouseOver(item) {
     .style("stroke-width", 3);
 }
 
-function handleMouseLeave() {
-  d3.selectAll(".itemValue").style("fill", "steelblue").attr("r", 2);
-}
+function handleMouseLeave(company) {
 
-function handleLineChartMouseLeave(item, color) {
-
+  color = randomColor()
   d3.selectAll(".itemValue").style("fill", "steelblue").attr("r", 2);
+  
+  d3.selectAll(".lineValues")
+    .filter(d => d.Company == company)
+    .style("fill", color)
+    .attr("r", 2);
   d3.selectAll(".lineTest")
     .filter(function (d, i) {
-      return d[0] == item[0];
+      return d[0] == company;
+    })
+    .attr("stroke", color)
+    .style("stroke-width", 1.5);
+}
+
+function handleLineChartMouseLeave(company, color) {
+
+  d3.selectAll(".itemValue").style("fill", "steelblue").attr("r", 2);
+
+  d3.selectAll(".lineValues")
+  .filter(d => d.Company == company)
+  .style("fill", color)
+  .attr("r", 2);
+
+
+  d3.selectAll(".lineTest")
+    .filter(function (d, i) {
+      return d[0] == company;
     })
     .attr("stroke", color)
     .style("stroke-width", 1.5);
@@ -297,4 +409,10 @@ function zIndex(company) {
   updateScatterPlot("vi4", "ROIC")
   updateScatterPlot("vi5", "Debt to Equity")
   updateScatterPlot("vi6", "priceBookValueRatio")
+}
+
+function randomColor() {
+  colors = ["blue", "steelblue", "orange", "yellow", "green", "purple", "red", "pink" ]
+  let random = Math.floor(Math.random() * 8);
+  return colors[random]
 }
